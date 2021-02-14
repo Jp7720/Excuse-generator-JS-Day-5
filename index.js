637c5dabdd64e75f39abc202f53cff402b1d82ae
jeffry porras <jeffryporrasbarahona@gmail.com>
2021-02-14 03:04:24 +0000

    trabajo terminado

---
+++ index.js
@@ -0,0 +1,24 @@
+function generator() {
+   
+var who = ['The cat','My grandma','His bunny','My bird'];
+var action = ['ate','peed','crushed','broke'];
+var  what = ['my homework', 'the keys', 'the car'];
+var when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+
+
+     
+        
+
+        var msg =
+          who[Math.floor(Math.random() * who.length)] +
+          " " +
+          what[Math.floor(Math.random() * what.length)] +
+          " " +
+          when[Math.floor(Math.random() * when.length)];
+          
+        document.getElementById("excuse").innerHTML = msg;
+       
+      
+
+
+}
