2273e409eabe47c566c0c20ea3a8dbb4169b8434
jeffry porras <jeffryporrasbarahona@gmail.com>
2021-02-14 06:14:01 +0000

    trabajo terminado

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 function generator() {
    
 var who = ['The cat','My grandma','His bunny','My bird'];
-var action = ['ate','peed','crushed','broke'];
 var  what = ['my homework', 'the keys', 'the car'];
 var when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
 
