b3cc33a82a93bd10afa1c484adfc44d9e92a732c
jeffry porras <jeffryporrasbarahona@gmail.com>
2021-03-04 20:05:19 +0000

    Finish 5

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function generator() {
    
 var who = ['The cat','My grandma','His bunny','My bird'];
+let action = ['ate','peed','crushed','broke'];
 var  what = ['my homework', 'the keys', 'the car'];
 var when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
 
@@ -10,6 +11,9 @@ var when = ['before the class','right on time','when I finished','during my lunc
 
         var msg =
           who[Math.floor(Math.random() * who.length)] +
+    
+          " " +
+          action[Math.floor(Math.random() * action.length)] +
           " " +
           what[Math.floor(Math.random() * what.length)] +
           " " +
@@ -17,7 +21,4 @@ var when = ['before the class','right on time','when I finished','during my lunc
           
         document.getElementById("excuse").innerHTML = msg;
        
-      
-
-
-}
+      }
